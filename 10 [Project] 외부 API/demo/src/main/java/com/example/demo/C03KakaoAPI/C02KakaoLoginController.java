444de1274e54760b6ca79dbc9c5f48b5800b544c
jungwoogyun <jwg8910@naver.com>
2023-08-30 19:23:25 +0900

    '.'

--- 10 [Project] 외부 API/demo/src/main/java/com/example/demo/C03KakaoAPI/C02KakaoLoginController.java
+++ 10 [Project] 외부 API/demo/src/main/java/com/example/demo/C03KakaoAPI/C02KakaoLoginController.java
@@ -17,13 +17,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
 @RequestMapping("/th/kakao")
 public class C02KakaoLoginController {
 
+//    private final String ADMIN_KEY = "APP_ADMIN_KEY";
     private final String CLIENT_ID = "746b917a629e6c3271acfaa6542552d5";
+
     private final String REDIRECT_URI = "http://localhost:8080/th/kakao/callback";
 
     private final String LOGOUT_REDIRECT_URI = "http://localhost:8080/th/kakao/login";
@@ -31,6 +34,7 @@ public class C02KakaoLoginController {
     //access-token/refresh-token/expires-in...
     private KakaoTokenResponse kakaoTokenResponse;
 
+    private KakaoProfile kakaoProfile;
 
     // 인가코드 요청 URL
     // https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}
@@ -38,6 +42,9 @@ public class C02KakaoLoginController {
     @GetMapping("/getCode")
     public void authorize(HttpServletResponse response) throws Exception {
         String url = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URI;
+        if(kakaoTokenResponse == null)
+            url += "&prompt=login";
+
         response.sendRedirect(url);
     }
 
@@ -112,6 +119,7 @@ public class C02KakaoLoginController {
         KakaoProfile response =  rt.postForObject(url,entity,KakaoProfile.class);
         System.out.println(response);
 
+        this.kakaoProfile = response;
         return response;
     }
     
@@ -162,23 +170,108 @@ public class C02KakaoLoginController {
         return null;
     }
 
-    // 카카오 계정과 함께 로그아웃
-    //로그아웃 Redirect URL 등록  // 카카오 계정과 함께 로그아웃(카카오 Application -> 카카오 로그인 -> 고급 -> LogoutRedirect 등록)
+    // 카카오 계정과 함께 로그아웃(카카오 서버에 저장된 사용자 액세스 토큰과 리프레시 토큰을 모두 만료)
+    // 개발 문서 : https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#logout
+
     @GetMapping("/logout")
-    public String logoutKakao(){
-        //URL
-        String url = "https://kauth.kakao.com/oauth/logout?client_id="+CLIENT_ID+"&logout_redirect_uri="+LOGOUT_REDIRECT_URI;
+    public String logoutKakao(HttpSession session){
+
         System.out.println("GET /th/kakao/logout");
-        return "forward:"+url;
+        //URL
+        String url = "https://kapi.kakao.com/v1/user/logout";
+        //Header
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/x-www-form-urlencoded");
+        headers.add("Authorization", "Bearer "+kakaoTokenResponse.getAccess_token());
+
+        //Parameter
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("target_id_type","user_id");
+        System.out.println("targetId : " + kakaoProfile.getId());
+        params.add("target_id",kakaoProfile.getId()+"");
+
+
+        //header + parameter
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params,headers);
+
+        //Request_Case1
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response =  rt.exchange(url, HttpMethod.POST,entity,String.class);
+
+        System.out.println(response);
+        System.out.println(response.getBody());
+
+        //세션 확인
+        String access_Token = (String)session.getAttribute("access_Token");
+        System.out.println("Session's accessToken : " + access_Token);
+
+
+        return "redirect:/th/kakao/logoutWithKakao";
+    }
+
+    // 카카오 계정과 함께 로그아웃(웹브라우저의 카카오정보 제거)
+    // 기술문서 : https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#logout-of-service-and-kakaoaccount
+
+    @GetMapping("/logoutWithKakao")
+    public String logoutKakaoWithKakao() throws Exception {
+
+        System.out.println("GET /th/kakao/logoutWithKakao");
+
+        //URL
+        String url = "https://kauth.kakao.com/oauth/logout?client_id="+ CLIENT_ID +"&logout_redirect_uri="+LOGOUT_REDIRECT_URI;
+
+
+        //Request_Case1
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response =  rt.exchange(url, HttpMethod.GET,null,String.class);
+
+        System.out.println(response);
+        System.out.println(response.getBody());
+
+
+        //초기화
+        this.kakaoProfile = null;
+        this.kakaoTokenResponse = null;
+        return "redirect:/th/kakao/login";
+
     }
 
 
-//    //APP 연결 끊기
-//    @GetMapping("/disConnect")
-//    public void disconnectKakao(){
-//
-//    }
-//
+    //APP 연결 끊기
+    @GetMapping("/disConnect")
+    public String disconnectKakao(){
+
+        System.out.println("GET /th/kakao/disConnect");
+
+        //URL
+        String url = "https://kapi.kakao.com/v1/user/unlink";
+        //Header
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/x-www-form-urlencoded");
+        headers.add("Authorization", "Bearer "+kakaoTokenResponse.getAccess_token());
+
+        //Parameter
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("target_id_type","user_id");
+        params.add("target_id",kakaoProfile.getId()+"");
+
+
+        //header + parameter
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params,headers);
+
+        //Request_Case1
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response =  rt.exchange(url, HttpMethod.POST,entity,String.class);
+
+        System.out.println(response);
+        System.out.println(response.getBody());
+
+
+        return "redirect:/th/kakao/logoutWithKakao";
+    }
+
 
 
 }
@@ -198,7 +291,7 @@ class KakaoTokenResponse {
 @Data
 class KakaoProfile {
     @JsonProperty("id")
-    private long id;
+    private Long id;
 
     @JsonProperty("connected_at")
     private String connected_at;
