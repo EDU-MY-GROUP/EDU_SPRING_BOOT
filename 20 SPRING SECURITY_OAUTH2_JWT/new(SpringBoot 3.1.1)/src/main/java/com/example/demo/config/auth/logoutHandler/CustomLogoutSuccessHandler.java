764fef5c8316f45e27349e06aeffc5ca6290c41f
jungwoogyun <jwg8910@naver.com>
2023-12-06 11:51:34 +0900

    .

--- 20 SPRING SECURITY_OAUTH2_JWT/new(SpringBoot 3.1.1)/src/main/java/com/example/demo/config/auth/logoutHandler/CustomLogoutSuccessHandler.java
+++ 20 SPRING SECURITY_OAUTH2_JWT/new(SpringBoot 3.1.1)/src/main/java/com/example/demo/config/auth/logoutHandler/CustomLogoutSuccessHandler.java
@@ -5,7 +5,6 @@ import com.example.demo.config.auth.PrincipalDetails;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.thymeleaf.util.StringUtils;
@@ -35,26 +34,26 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 		System.out.println("CustomLogoutSuccessHandler's onLogoutSuccess! authentication : " + authentication);
 
 
-//		PrincipalDetails principalDetails =  (PrincipalDetails) authentication.getPrincipal();
-//		String provider = principalDetails.getUser().getProvider();
-//
-//		if(provider!=null&&StringUtils.contains(provider,"kakao")){
-//			String url2 = "https://kauth.kakao.com/oauth/logout?client_id="+kakaoCliendId+"&logout_redirect_uri="+LOGOUT_REDIRECT_URI;
-//			response.sendRedirect(url2);
-//			return ;
-//		}
-//		else if(provider!=null&&StringUtils.contains(provider,"naver"))
-//		{
-//			String url = "http://nid.naver.com/nidlogin.logout"; //네이버 APP 연결끊기
-//			response.sendRedirect(url);
-//			return ;
-//		}
-//		else if(provider!=null&&StringUtils.contains(provider,"google"))
-//		{
-//			String url = "https://accounts.google.com/Logout";
-//			response.sendRedirect(url);
-//			return;
-//		}
+		PrincipalDetails principalDetails =  (PrincipalDetails) authentication.getPrincipal();
+		String provider = principalDetails.getUser().getProvider();
+
+		if(provider!=null&&StringUtils.contains(provider,"kakao")){
+			String url2 = "https://kauth.kakao.com/oauth/logout?client_id="+kakaoCliendId+"&logout_redirect_uri="+LOGOUT_REDIRECT_URI;
+			response.sendRedirect(url2);
+			return ;
+		}
+		else if(provider!=null&&StringUtils.contains(provider,"naver"))
+		{
+			String url = "http://nid.naver.com/nidlogin.logout"; //네이버 APP 연결끊기
+			response.sendRedirect(url);
+			return ;
+		}
+		else if(provider!=null&&StringUtils.contains(provider,"google"))
+		{
+			String url = "https://accounts.google.com/Logout";
+			response.sendRedirect(url);
+			return;
+		}
 
 		response.sendRedirect( request.getContextPath() );	// '/' 경로로 이동
 
